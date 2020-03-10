96ae6bde2418f00dbeb692013bf99ab5f88c87a4
Felipe Giambona <felipegia@gmail.com>
2020-03-10 17:00:32 -0300

    erro.js

---
+++ fundamentos/erro.js
@@ -0,0 +1,24 @@
+function tratatErroELancar(erro) {
+    // throw new error('...')
+    // throw 10
+    // throw true
+    // throw 'mensagem
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date()
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratatErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { nome: 'Roberto'}
+imprimirNomeGritado(obj)
